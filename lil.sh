07887ec9eda388efc204061d122f1db34c6e6e0d
crypto 0834 <cryptoheist834@gmail.com>
2024-09-06 06:21:07 +0000

    feat: initiated the project

--- lil.sh
+++ lil.sh
@@ -4,6 +4,7 @@ wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guid
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
 sleep 4
 
+
 sudo apt-get update && sudo apt-get upgrade -y
 clear
 
